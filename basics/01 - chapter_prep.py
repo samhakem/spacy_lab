99f2a652baa08e2b90580c9b1d55c9bd64276b18
Sam Hakem <samhakem@egmail.com>
2022-06-01 12:38:46 +0200

    first commit and structure setup

--- basics/01 - chapter_prep.py
+++ basics/01 - chapter_prep.py
@@ -1,12 +1,12 @@
 import spacy
-import os
+# import os
 
 with open('/home/samhakem/PycharmProjects/spacy_lab/basics/data/alice.txt', 'r') as f:
     text = f.read()
-    #print(text)
+    # print(text)
     chapters = text.split('CHAPTER ')[1:]  # Start from second index
     print(chapters[0])  # Print any chapter by indexing 0 = 1 etc
-    #print(len(chapters))  # Confirm there are 12 chapters in book
-    #print(os.getcwd())  # Confirm working directory for f reads
+    # print(len(chapters))  # Confirm there are 12 chapters in book
+    # print(os.getcwdu())  # Confirm working directory for f reads
 
 nlp = spacy.load('en_core_web_sm')

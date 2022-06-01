d70a06d27046725a572450191bb36648b4912b05
Sam Hakem <samhakem@egmail.com>
2022-06-01 13:08:31 +0200

    first commit and structure setup

--- basics/01 - chapter_prep.py
+++ basics/01 - chapter_prep.py
@@ -20,5 +20,22 @@ nlp = spacy.load('en_core_web_sm')
 doc = nlp(chapter1)
 
 # Separate out doc out by sents
-# sentences = list(doc.sents)
+sentences = list(doc.sents)
+sentence = (sentences[2])
 # print(sentences[1])
+
+ents = list(sentence.ents)  # Breaks down sentence object and turns it into a spaCy and will look for all named
+# ents = list(doc.ents)  # Test and deal with false positives across whole doc
+# entities in text
+
+people = []
+
+for ent in ents:
+    if ent.text.label_ == 'PERSON':
+        people.append(ent)
+
+print(people)
+
+print(ents[0].label)  # [Alice] Numerical value of object
+print(ents[0].label_)  # [PERSON] Actual entity
+print(ents[0].text)  # Object string

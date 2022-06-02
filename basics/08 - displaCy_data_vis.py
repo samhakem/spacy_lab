fb24f87ad2ab3a9cf0f3bce640ca3258a388e26d
Sam Hakem <samhakem@egmail.com>
2022-06-02 16:31:39 +0200

    first commit and structure setup

--- basics/08 - displaCy_data_vis.py
+++ basics/08 - displaCy_data_vis.py
@@ -3,16 +3,15 @@ from spacy import displacy
 
 # import os
 
-with open('/home/samhakem/PycharmProjects/spacy_lab/basics/data/alice.txt', 'r') as f:
-    # Remove line breaks and double line breaks to allow
-    # small 'model to accurately read in sentences with \n
-    text = f.read().replace('\n\n', ' ').replace('\n', ' ')
+# with open('/home/samhakem/PycharmProjects/spacy_lab/basics/data/alice.txt', 'r') as f:  # Remove line breaks and
+# double line breaks to allow small 'model to accurately read in sentences with \n
+# text: object = f.read().replace('\n\n', ' ').replace('\n', ' ')
 
-    # print(text)
-    chapters = text.split('CHAPTER ')[1:]  # Start from second index
-    # print(chapters[0])  # Print any chapter by indexing 0 = 1 etc
-    # print(len(chapters))  # Confirm there are 12 chapters in book
-    # print(os.getcwdu())  # Confirm working directory for f reads
+# print(text)
+# chapters = text.split('CHAPTER ')[1:]  # Start from second index
+# print(chapters[0])  # Print any chapter by indexing 0 = 1 etc
+# print(len(chapters))  # Confirm there are 12 chapters in book
+# print(os.getcwdu())  # Confirm working directory for f reads
 
 chapter1 = chapters[0]
 
@@ -24,9 +23,9 @@ sentences = list(doc.sents)
 sentence = (sentences[6])
 
 html = displacy.render(sentence, style='dep')  # Render saves the visualisation
-html = displacy.render(sentence, style='ent')
+# html = displacy.render(sentence, style='ent')
 
-html = displacy.serve(sentence, style='dep')  # Serve only displays it
+# html = displacy.serve(sentence, style='dep')  # Serve only displays it
 
 with open('data_vis.html', 'w') as f:  # Saves the visualization to an HTML file locally
     f.write(html)
